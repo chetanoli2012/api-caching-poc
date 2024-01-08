8d69ade7b2290cbdcda7671ab12aaa53891add52
Chetan Oli <chetanoli@EMPID23052.local>
2024-01-08 14:42:10 +0545

    Minor code refactoring

---
+++ constants/constants.js
@@ -0,0 +1,1 @@
+export const PUNK_API_BASE_URL = "https://api.punkapi.com/v2/beers";
--- hooks/useBeerData.js
+++ hooks/useBeerData.js
@@ -1,10 +1,11 @@
 import useSWR from "swr";
+import { PUNK_API_BASE_URL } from "../constants/constants";
 
 export const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export const useBeerData = (page) => {
   const { data, error } = useSWR(
-    `https://api.punkapi.com/v2/beers?page=${page}&per_page=10`,
+    `${PUNK_API_BASE_URL}?page=${page}&per_page=10`,
     fetcher
   );
 
